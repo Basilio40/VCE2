25f8cf0e556766a69b7c83f8b5cf00f7ace9a258
Basilio40 <42722896+Basilio40@users.noreply.github.com>
2021-08-29 23:56:43 -0300

    inserido config heroku

--- Doc_VCE/wsgi.py
+++ Doc_VCE/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Doc_VCE.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Doc_VCE.settings.base')
 
 application = get_wsgi_application()
