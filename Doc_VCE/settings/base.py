9c9a119a34f97928adbef29d46c9f0fcb0104a83
Basilio40 <sbispo@otimaeg.com.br>
2022-04-13 15:55:56 +0000

    copiando arquivos

--- Doc_VCE/settings/base.py
+++ Doc_VCE/settings/base.py
@@ -89,20 +89,10 @@ WSGI_APPLICATION = 'Doc_VCE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'doc_vce',
-        'USER' : 'doc_vce',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
